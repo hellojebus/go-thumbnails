d2c96282f506be0c94832e3e71c714a2459d2078
Jesus Lizama <dev@hellojeb.us>
2018-05-28 21:31:47 -0700

    run program on test jpg

--- main.go
+++ main.go
@@ -8,11 +8,21 @@ import (
 	"bufio"
 	"os"
 	"github.com/hellojebus/go-thumbnails/thumbnail"
+	"log"
+	"fmt"
 )
 
 func main() {
 	input := bufio.NewScanner(os.Stdin)
 	for input.Scan() {
-		thumb, err := thumbnail.F
+		thumb, err := thumbnail.ImageFile(input.Text())
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		fmt.Println(thumb)
+	}
+	if err := input.Err(); err != nil {
+		log.Fatal(err)
 	}
 }
