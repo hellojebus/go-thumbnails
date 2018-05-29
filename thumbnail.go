31371152ec7bf738c79513cb6f9523b05aa8b626
Jesus Lizama <dev@hellojeb.us>
2018-05-28 21:12:09 -0700

    add image func

--- thumbnail.go
+++ thumbnail.go
@@ -1,3 +1,30 @@
 package thumbnail
 
+import "image"
 
+// get thumbnail size of src
+func Image(src image.Image) image.Image {
+	// get size of image
+	xs := src.Bounds().Size().X
+	ys := src.Bounds().Size().Y
+	width, height := 128, 128
+	if aspect := float64(xs) / float64(ys); aspect < 1.0 {
+		width = int(128 * aspect) // portrait
+	} else {
+		height = int(128 / aspect) // landscape
+	}
+	xscale := float64(xs) / float64(width)
+	yscale := float64(ys) / float64(height)
+
+	dst := image.NewRGBA(image.Rect(0, 0, width, height))
+
+	// scaling algo
+	for x := 0; x < width; x++ {
+		for y := 0; y < height; y++ {
+			srcx := int(float64(x) * xscale)
+			srcy := int(float64(y) * yscale)
+			dst.Set(x, y, src.At(srcx, srcy))
+		}
+	}
+	return dst
+}
